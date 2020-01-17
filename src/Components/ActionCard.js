6a25c068e2c4cdae6c73925d0be41e3bdaf74871
mezzolite <mez.charney@gmail.com>
2020-01-17 11:25:02 -0700

    adds complete button to tasks

--- src/Components/ActionCard.js
+++ src/Components/ActionCard.js
@@ -1,15 +1,19 @@
 import React from 'react'
 import { makeStyles } from '@material-ui/core/styles';
 import Popover from '@material-ui/core/Popover';
+import DoneOutlineRoundedIcon from '@material-ui/icons/DoneOutlineRounded';
 
 const useStyles = makeStyles(theme => ({
     typography: {
       padding: theme.spacing(2),
     },
-    style: {
+    popoverStyle: {
         padding: theme.spacing(2),
         backgroundColor: '#F9EBE0',
         color: '#73956F'
+    },
+    doneStyle: {
+        color: '#FF8A78'
     }
   }));
 
@@ -25,13 +29,19 @@ const ActionCard = ({action}) => {
         setAnchorEl(null);
     };
 
+    const doneButtonClick = () => {
+        
+    }
+
     const open = Boolean(anchorEl);
     const id = open ? 'simple-popover' : undefined;
 
     return(
         <div className="action-card" >
-            <li aria-describedby={id} onClick={handleClick}>{action.title}</li>
-            <p>Reward: {action.reward} points</p>
+            <li aria-describedby={id} onClick={handleClick}>{action.title}
+                <p>Reward: {action.reward} points</p>
+            </li>
+            <button><DoneOutlineRoundedIcon className={classes.doneStyle}/></button>
             <Popover
                 id={id}
                 open={open}
@@ -46,7 +56,7 @@ const ActionCard = ({action}) => {
                     horizontal: 'center',
                 }}
             >
-                <div className={classes.style}>
+                <div className={classes.popoverStyle}>
                     <p>{action.description}</p>
                     <a href={action.link} target="_blank" rel="noopener noreferrer">Action Info Here</a>
 
